85e52bf42430d3c695d82fe5cc62bda9bcdb7bd2
Leonardo <leon-97@live.it>
2021-06-18 13:52:58 +0200

    impl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const API_KEY = '793525d8'
+const API_URL = 'http://www.omdbapi.com'
+
+function ricercaFilm(params = 'back to the past') {
+  return fetch(API_URL + '?apikey=' + API_KEY + '&s=' + params).then(res => res.json())
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      film: [],
+      totali: 0
+    }
+  }
+
+  componentDidMount() {
+    ricercaFilm().then(res => {
+      this.setState({
+        film: res.Search
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>MY MOVIES</h1>
+        <ul>
+          {this.state.film.map(film => <li key={film.imdbID}>{film.Title}</li>)}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
